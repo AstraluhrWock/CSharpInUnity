5df515c26485241f1e7f8369c31a712cde387293
Lubov Sokolova <falke.liebe@gmail.com>
2022-10-24 11:23:49 +0800

    add covariance

---
+++ Assets/Scripts/CovarianceDelegate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CovarianceDelegate : MonoBehaviour
+{
+    class Bonus { }
+    class BadBonus : Bonus { }
+   // class GoodBonus : Bonus { }
+
+    delegate Bonus Bonusdelegate();
+
+    static void Main(string[] args)
+    {
+        // ковариантность
+        Bonusdelegate del1 = BonusMethod;
+        del1();
+       
+
+        Bonusdelegate del2 = BadBonusMethod;
+        del2();
+    }
+    
+
+    static Bonus BonusMethod()
+    {
+        Debug.Log("Main method");
+        return new Bonus();
+    }
+
+    static BadBonus BadBonusMethod()
+    {
+        Debug.Log("Bad bonus method");
+        return new BadBonus();
+    }
+
+   /* static GoodBonus GoodBonusMethod()
+    {
+        Debug.Log("Good bonus method");
+        return new GoodBonus();
+    }    */
+
+
+}
---
+++ Assets/Scripts/SaveData/Save.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+
+
+public class Save : MonoBehaviour
+{
+    private SerializableXML<SavedData> _serializibleXMLData = new SerializableXML<SavedData>();
+    private SavedData _savedData;
+
+    public void Start()
+    {
+        var bonus = GameObject.FindGameObjectWithTag("Player");
+        _savedData = new SavedData() { Name = bonus.name, Position = bonus.transform.position };
+        var path = Path.Combine(Application.streamingAssetsPath, "SerializableXmlSave.xml");
+        _serializibleXMLData.Save(_savedData,path);
+       
+    }
+}
+ 
